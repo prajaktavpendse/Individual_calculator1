81a957ab2c47ffc120086aba97222ad578022142
Prajakta <prajaktavpendse@gmail.com>
2019-11-04 16:13:46 -0500

    Feature: Added parse function

--- scalculator.py
+++ scalculator.py
@@ -53,3 +53,55 @@ class Calculator:
             return Calculator.square(nums[0])
         elif operation == Calculator.Operation.SQUARE_ROOT:
             return Calculator.square_root(nums[0])
+
+        # Applies the given operation for each line of the file
+        def load_from_file_and_compute(self, file_name, separator, operation_str, is_header_available=False):
+            operation = Calculator.get_operation(operation_str)
+            if operation is Calculator.Operation.UNKNOWN:
+                print("Invalid operation")
+                exit(-1)
+
+            for values_list in Calculator.open_file_and_iterate_per_line(file_name, separator, is_header_available):
+                if not values_list:
+                    continue
+
+                values_list = [self.parse_number(str(v), False) for v in values_list]
+
+                if any([math.isnan(x) for x in values_list]):
+                    print("Invalid inputs detected, skipping this line")
+                    continue
+
+                self.result = values_list[0]
+
+                for num in range(1, len(values_list)):
+                    actual_num = values_list[num]
+                    self.result = Calculator.dispatch_and_compute_result(self.result, actual_num)
+
+                # Print the value
+                print("Numbers: {}, result: {}".format(values_list, self.result))
+
+        def parse_number(self, numberStr, allow_res=True):
+            if numberStr == "res" and allow_res:
+                return self.result
+            else:
+                try:
+                    if "." in numberStr:
+                        return float(numberStr)
+                    else:
+                        return int(numberStr)
+                except:
+                    return float("nan")
+
+        def parse_interactive_mode_input(self, inputStr, get_oper_function):
+            tokens = inputStr.split()
+            if len(tokens) < 2:
+                return []
+            else:
+                operationn = get_oper_function(tokens[-1])
+                nums = [self.parse_number(n) for n in tokens[:-1]]
+
+            for num in nums:
+                if math.isnan(num):
+                    return []
+
+            return [operationn, nums]
