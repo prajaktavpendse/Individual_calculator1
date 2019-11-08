2925991ef6e24b26fe8db6e263e984ea693ff87a
Prajakta <prajaktavpendse@gmail.com>
2019-11-07 19:26:54 -0500

    Feature: Added square and square root operation

--- scalculator.py
+++ scalculator.py
@@ -19,24 +19,6 @@ class Calculator:
         self.result = float("nan")
 
     @staticmethod
-    def open_file_and_iterate_per_line(file_name, separator, skip_header=False):
-        with open(file_name, "r") as file:
-            line_str = file.readline()
-
-            if not line_str:
-                yield list()
-                return
-
-            if skip_header:
-                line_str = file.readline()
-
-            while line_str:
-                values = line_str.split(separator)
-                yield values
-
-                line_str = file.readline()
-
-    @staticmethod
     def dispatch_and_compute_result(operation, nums):
         if isinstance(operation, str):
             operation = Calculator.get_operation(operation)
@@ -54,65 +36,38 @@ class Calculator:
         elif operation == Calculator.Operation.SQUARE_ROOT:
             return Calculator.square_root(nums[0])
 
-        # Applies the given operation for each line of the file
-        def load_from_file_and_compute(self, file_name, separator, operation_str, is_header_available=False):
-            operation = Calculator.get_operation(operation_str)
-            if operation is Calculator.Operation.UNKNOWN:
-                print("Invalid operation")
-                exit(-1)
+    def parse_number(self, numberStr, allow_res=True):
+        if numberStr == "res" and allow_res:
+            return self.result
+        else:
+            try:
+                if "." in numberStr:
+                    return float(numberStr)
+                else:
+                    return int(numberStr)
+            except:
+                return float("nan")
 
-            for values_list in Calculator.open_file_and_iterate_per_line(file_name, separator, is_header_available):
-                if not values_list:
-                    continue
+    def parse_interactive_mode_input(self, inputStr, get_oper_function):
+        tokens = inputStr.split()
+        if len(tokens) < 2:
+            return []
+        else:
+            operationn = get_oper_function(tokens[-1])
+            nums = [self.parse_number(n) for n in tokens[:-1]]
 
-                values_list = [self.parse_number(str(v), False) for v in values_list]
-
-                if any([math.isnan(x) for x in values_list]):
-                    print("Invalid inputs detected, skipping this line")
-                    continue
-
-                self.result = values_list[0]
-
-                for num in range(1, len(values_list)):
-                    actual_num = values_list[num]
-                    self.result = Calculator.dispatch_and_compute_result(self.result, actual_num)
-
-                # Print the value
-                print("Numbers: {}, result: {}".format(values_list, self.result))
-
-        def parse_number(self, numberStr, allow_res=True):
-            if numberStr == "res" and allow_res:
-                return self.result
-            else:
-                try:
-                    if "." in numberStr:
-                        return float(numberStr)
-                    else:
-                        return int(numberStr)
-                except:
-                    return float("nan")
-
-        def parse_interactive_mode_input(self, inputStr, get_oper_function):
-            tokens = inputStr.split()
-            if len(tokens) < 2:
+        for num in nums:
+            if math.isnan(num):
                 return []
-            else:
-                operationn = get_oper_function(tokens[-1])
-                nums = [self.parse_number(n) for n in tokens[:-1]]
 
-            for num in nums:
-                if math.isnan(num):
-                    return []
-
-            return [operationn, nums]
+        return [operationn, nums]
 
     def process_input_data(self, input_str, parse_operation_func=None):
         if not parse_operation_func:
             parse_operation_func = Calculator.get_operation
         input_data = self.parse_interactive_mode_input(input_str, parse_operation_func)
         if not input_data:
-            print("You entered invalid data!")
-            exit(-1)
+            raise ValueError("You entered invalid data!")
         return input_data
 
     def start_interactive_calculator(self):
@@ -125,7 +80,8 @@ class Calculator:
               "(5) num1 SQUARE\n"
               "(6) num1 SQRT\n"
               "(7) Use res to use results of computations till now\n"
-              "Example: res ADD 5")
+              "Example: res ADD 5\n"
+              "Hit Enter to display result and exit")
 
         input_str = input("Enter input: ")
         while input_str:
@@ -160,7 +116,7 @@ class Calculator:
     @staticmethod
     def square_root(num1):
         if num1 < 0:
-            print("Invalid number")
+            raise ValueError("Invalid number")
         return math.sqrt(num1)
 
     @staticmethod
@@ -187,3 +143,6 @@ class Calculator:
             return Calculator.Operation.UNKNOWN
 
 
+if __name__ == '__main__':
+    calculator = Calculator()
+    calculator.start_interactive_calculator()
